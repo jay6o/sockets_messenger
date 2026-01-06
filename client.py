642b4f12debaea4bf6a80c49a84f8d065947c191
jay6o <richforeverrr6@proton.me>
2026-01-06 15:06:38 -0500

    added coloring and formatting

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
+import sys
 import socket
 import threading
+from colorama import Fore, Back, Style
 
 def connect(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,17 +21,30 @@ def recv_message(s, user):
             print("Server disconnected, exiting.")
             s.close()
             exit(0)
-        print(f"{r} ", flush=True)
+        i = 0
+        while r[i] != ':':
+            i += 1
+        sender = r[:i]
+        msg = r[i+1:]
+        
+        sys.stdout.write('\r' + ' ' * 80 + '\r')
+        sys.stdout.flush()
+
+        print("")
+        print(f"\r{Fore.BLUE}{sender}{Style.RESET_ALL}:{msg}")
+        print("")
+        print("> ", end="", flush=True)
 
 if __name__ == "__main__":
     try:
         s, user = connect('localhost', 5001)
+        print(f"Connected to server as {user}. Type 'exit' or 'quit' to leave.\n\n")
         recv_thread = threading.Thread(target=recv_message, args=(s, user))
         recv_thread.daemon = True
         recv_thread.start()
 
         while 1:
-            message = input(f"")
+            message = input("> ")
             if message.strip().lower() == "exit" or message.strip().lower() == "quit":
                 print("Exiting.")
                 s.close()
